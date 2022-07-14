a8d04a323a23a93900de3255645e2a6cc9ecfc6a
Ankit Wadhwa <ankitwadhwa1402.com>
2022-07-15 01:20:53 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,56 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
+const app = express();
+dotenv.config();
+
+
+import path from 'path';
+// import {fileURLToPath} from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+ app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/',(req,res)=>{
+  res.send({message:"hello"}).json();
+}
+);
+
+
+
+
+
+const CONNECTION_URL = 'mongodb+srv://ankit:ankit123@cluster0.lsjskkj.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 5000;
+const URL=CONNECTION_URL;
+// if(process.env.NODE_ENV=="production")
+// {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+// });
+
+// }
+
+
+app.use(express.static(path.join(__dirname, 'client','build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+});
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
