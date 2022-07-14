d3b1c59fb25dd12954231ad4208b9e8a7504bbcd
Ankit Wadhwa <ankitwadhwa1402.com>
2022-07-15 01:46:22 +0530

    better

---
+++ client/build/precache-manifest.6d73ceacc5b26a5b9b6d5f3734c09d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00dee273d9e157dc442ecd26ede31519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c67f6fb3845fbca5f1",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c67f6fb3845fbca5f1",
+    "url": "/static/js/main.f7af77e3.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
