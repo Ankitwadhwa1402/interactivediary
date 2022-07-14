b5cf1e1c433f2847a23a6fbe57bea73a70abe63a
Ankit Wadhwa <ankitwadhwa1402.com>
2022-07-15 01:58:37 +0530

    change

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
  app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/',(req,res)=>{
-  res.send({message:"hello"}).json();
-}
-);
+// app.use('/',(req,res)=>{
+//   res.send({message:"hello"}).json();
+// }
+// );
 
 
 
 
 
-const CONNECTION_URL = 'mongodb+srv://ankit:ankit123@cluster0.lsjskkj.mongodb.net/?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://ankit:ankit123@cluster0.lsjskkj.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
 // if(process.env.NODE_ENV=="production")
@@ -50,7 +50,7 @@ app.use(express.static(path.join(__dirname, 'client','build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
 });
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT || 3000 , () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
